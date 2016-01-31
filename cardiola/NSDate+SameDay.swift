f772dc1815a1758e9ad59aafd8500a664deee69e
Janusch Jacoby <janusch.jacoby@student.hpi.uni-potsdam.de>
2016-01-31 21:36:35 +0100

    Add utilities and extensions

---
+++ cardiola/NSDate+SameDay.swift
@@ -0,0 +1,45 @@
+//
+//  NSDate+SameDay.swift
+//  cardiola
+//
+//  Created by Janusch Jacoby on 31/01/16.
+//  Copyright © 2016 BPPolze. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate {
+    
+    var day: Int {
+        let components = self.getAllComponentsForCurrentCalendar()
+        return components.day
+    }
+    
+    var month: Int {
+        let components = self.getAllComponentsForCurrentCalendar()
+        return components.month
+    }
+    
+    var year: Int {
+        let components = self.getAllComponentsForCurrentCalendar()
+        return components.year
+    }
+    
+    func isSameDayAs(date otherDate: NSDate) -> Bool {
+        let calendar = NSCalendar.currentCalendar()
+        let unitFlags = NSCalendarUnit(rawValue: UInt.max)
+        let comp1 = calendar.components(unitFlags, fromDate: self)
+        let comp2 = calendar.components(unitFlags, fromDate: otherDate)
+        
+        return comp1.day == comp2.day
+            && comp1.month == comp2.month
+            && comp1.year == comp2.year
+    }
+    
+    func getAllComponentsForCurrentCalendar() -> NSDateComponents {
+        let calendar = NSCalendar.currentCalendar()
+        let unitFlags = NSCalendarUnit(rawValue: UInt.max)
+        return calendar.components(unitFlags, fromDate: self)
+    }
+    
+}
---
+++ cardiola/String+Subscript.swift
@@ -0,0 +1,24 @@
+//
+//  String+Subscript.swift
+//  cardiola
+//
+//  Created by Janusch Jacoby on 31/01/16.
+//  Copyright © 2016 BPPolze. All rights reserved.
+//
+
+extension String {
+    
+    subscript (i: Int) -> Character {
+        return self[self.startIndex.advancedBy(i)]
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = startIndex.advancedBy(r.startIndex)
+        let end = start.advancedBy(r.endIndex - r.startIndex)
+        return self[Range(start: start, end: end)]
+    }
+}
